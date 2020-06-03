c5aa702ad71da7c8141935e4076461d19610dcff
chocoboii~ <63813281+defoko-bot@users.noreply.github.com>
2020-06-03 14:57:24 -0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+const file = require("./msg.json");
+const lemsgs = file.messages;
+
+client.on("error", (e) => {
+    console.log(e)
+});
+
+client.on("ready", () => {
+    console.log("vivaaaaaaaaa")
+    client.user.setActivity("ouvindo suas msgs, sdds :pray: / -padero");
+});
+
+client.on("message", (msg) => {
+    if(msg.author.bot || msg.mentions.members.first()) return;
+    if(msg.channel.id === `716808248271044661`){
+        lemsgs.push(msg.content);
+        const logger = fs.createWriteStream("./msg.json")
+        logger.write(JSON.stringify(file))
+    }
+    if(msg.channel.id === `717734564126130218`){;
+        const randommsg = lemsgs[Math.floor(Math.random()*lemsgs.length)];
+        msg.channel.send(randommsg)
+        console.log(`Mensagem ${randommsg} enviada`)
+    }
+});
+
+client.login(process.env.token)
